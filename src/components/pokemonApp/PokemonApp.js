a1426500ee317136d24df3024b8ad996319d6536
jas112 <jas.sandmaster@gmail.com>
2022-09-16 01:01:52 -0700

    axios and uuid added...groundwork set for data retrieval.

--- src/components/pokemonApp/PokemonApp.js
+++ src/components/pokemonApp/PokemonApp.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function PokemonApp() {
+    const [pokemonData, setPokemonData] = useState ([]);
+
+    useEffect(() => {
+
+        async function fetchPokemonData(){
+            const response = await axios.get(``);
+            console.log(response.data);
+            setPokemonData(response.data);
+        }
+
+        fetchPokemonData();
+
+    }, [pokemonData]);
+
   return (
     <div>
         <h1>Welcome to PokemonApp!!!</h1>
