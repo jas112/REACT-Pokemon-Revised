3e6278ef53cb496a02957089d2642ceaf02d2682
jas112 <jas.sandmaster@gmail.com>
2022-09-21 22:53:04 -0700

    entries made to usePokemonState js.

--- src/hooks/usePokemonState.js
+++ src/hooks/usePokemonState.js
@@ -1,15 +1,18 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import useLocalStorageState from './useLocalStorageState';
 
 
 function usePokemonState(initialPokemon) {
 
-    const [pokemon, setPokemon] = useLocalStorageState('tasks', initialPokemon);
+    const [pokemon, setPokemonData] = useLocalStorageState('pokemonData', initialPokemon);
 
     return {
         pokemon,
-        getPokemon: () => {
-            
+        getPokemon: async () => {
+            const response1 = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=300&offset=0`);
+            setPokemonData(response1.data.results);
+            return response1.data.results;
         }
     };
 
